b989e174e20a2a6a34233ff8a2a575201e671e48
Gboyega Dada <boyega@gmail.com>
2024-04-09 05:45:53 +0100

    Initial Commit

---
+++ FocusedValueIssue/FocusedValueIssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusedValueIssueApp.swift
+//  FocusedValueIssue
+//
+//  Created by Gboyega Dada on 09/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct FocusedValueIssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
